c95afc980ef4ea9ed108e56f0e791a2d3f7b2892
victor-kofa <victor.owusu@kofa.co>
2025-09-12 11:25:09 +0000

    images icons const

---
+++ app/shared/constants/images.ts
@@ -0,0 +1,35 @@
+export const LOGO = "/assets/kofa.png";
+
+export const SIDEBAR = "/assets/sidebar.png";
+
+export const DASHBOARD = "/assets/dashboard.png";
+
+export const RIDERS = "/assets/users.png";
+
+export const MOTORBIKE = "/assets/motorbike.png";
+
+export const COMPASS = "/assets/compass.png";
+
+export const ADMIN = "/assets/admin.png";
+
+export const SEARCH = "/assets/search.png";
+
+export const CHECKBOX = "/assets/check.png";
+
+export const ACCESS = "/assets/access.png";
+
+export const RIDER = "/assets/user.png";
+
+export const RIDERHEADING = "/assets/riderTableHeading.png";
+
+export const TABLEPHONE = "/assets/phone.png";
+
+export const LASTSWAP = "/assets/last-swap.png";
+
+export const MOREACTIONS = "/assets/more-actions.png";
+
+export const GREENCHECK = "/assets/green-check.png";
+
+export const REDCLOSE = "/assets/red-close.png";
+
+export const PLUSMINUS = "/assets/plus-minus.png";
