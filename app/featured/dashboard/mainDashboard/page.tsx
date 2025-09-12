4303c7b65134c2bfbdd65640d7db46d28a726256
victor-kofa <victor.owusu@kofa.co>
2025-09-12 11:20:50 +0000

    import dashboard main page

---
+++ app/featured/dashboard/mainDashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import DashboardTable from "@/app/shared/components/DashboardTable";
+
+export default function MainDashboard() {
+  return (
+    <div className="mt-[7rem] container mx-auto mainDashboard">
+      <DashboardTable />
+    </div>
+  );
+}
