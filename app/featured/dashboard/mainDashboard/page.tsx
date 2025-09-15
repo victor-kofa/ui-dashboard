c58cd75f582d5a17e28b0f41763fb35b13c2c582
victor-kofa <victor.owusu@kofa.co>
2025-09-15 16:00:07 +0000

    refactor Header to SearchAdd

--- app/featured/dashboard/mainDashboard/page.tsx
+++ app/featured/dashboard/mainDashboard/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 import DashboardTable from "@/app/shared/components/DashboardTable";
-import Header from "../components/SearchAdd";
+import SearchAdd from "../components/SearchAdd";
 
 export default function MainDashboard() {
   return (
-    <div className="mt-[24px] mx-[24px] mainDashboard">
-      <Header />
+    <div className="mt-[24px] mx-[24px] mainDashboard bg-amber-500">
+      <SearchAdd />
       <DashboardTable />
     </div>
   );
