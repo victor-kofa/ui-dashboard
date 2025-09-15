88291150b0401f664d09aac3ed1c3929595a0a86
victor-kofa <victor.owusu@kofa.co>
2025-09-15 15:31:42 +0000

    24 top and sides

--- app/featured/dashboard/mainDashboard/page.tsx
+++ app/featured/dashboard/mainDashboard/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import DashboardTable from "@/app/shared/components/DashboardTable";
+import Header from "../components/SearchAdd";
 
 export default function MainDashboard() {
   return (
-    <div className="mt-[7rem] container mx-auto mainDashboard">
+    <div className="mt-[24px] mx-[24px] mainDashboard">
+      <Header />
       <DashboardTable />
     </div>
   );
