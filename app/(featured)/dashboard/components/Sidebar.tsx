01edce28fcfe54025d0d8022c24a453f6264d547
victor-kofa <victor.owusu@kofa.co>
2025-09-16 09:59:10 +0000

    use sidebar icon

---
+++ app/(featured)/dashboard/components/Sidebar.tsx
@@ -0,0 +1,66 @@
+import { AdminIcon } from "@/app/shared/components/AdminIcon";
+import { CompassIcon } from "@/app/shared/components/CompassIcon";
+import { DashboardImage } from "@/app/shared/components/DashboardImage";
+import MenuIcon from "@/app/shared/components/MenuIcon";
+import { MotorbikeIcon } from "@/app/shared/components/MotorbikeIcon";
+import { ProjectIcon } from "@/app/shared/components/ProjectIcon";
+import { ProjectImage } from "@/app/shared/components/ProjectImage";
+import { RidersIcon } from "@/app/shared/components/RidersIcon";
+import {
+  ADMIN,
+  COMPASS,
+  DASHBOARD,
+  LOGO,
+  MENUICON,
+  MOTORBIKE,
+  RIDERS,
+  SIDEBAR,
+} from "@/app/shared/constants/images";
+import Link from "next/link";
+import React from "react";
+
+function Sidebar() {
+  return (
+    <>
+      <div className="mainSidebar">
+        <div className="flex items-center justify-between">
+          {/* Logo */}
+          <Link href="/" className="logo">
+            <ProjectImage
+              imageHeight={120}
+              imageWidth={120}
+              className=""
+              alt="logo"
+              path={LOGO}
+            />
+          </Link>
+
+          {/* SIdebar */}
+          <Link href="/" className="">
+            <ProjectIcon
+              iconWidth={20}
+              iconHeight={20}
+              className=""
+              alt="logo"
+              path={SIDEBAR}
+            />
+          </Link>
+        </div>
+
+        {/* Dashboard Profile */}
+
+        <div className="sidebarBox ">
+          <MenuIcon
+            iconWidth={210}
+            iconHeight={220}
+            className=""
+            alt="logo"
+            path={MENUICON}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
