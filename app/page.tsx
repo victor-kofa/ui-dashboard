96c388db77e0a27ad26ac6fc6a45b2be11055f9e
victor-kofa <victor.owusu@kofa.co>
2025-09-16 09:29:59 +0000

    import sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import Dashboard from "./featured/dashboard/page";
+import Sidebar from "./(featured)/dashboard/components/Sidebar";
+import Dashboard from "./(featured)/dashboard/page";
 
 export default function Home() {
   return (
-    <section className="flex justify-between">
+    <section className="flex max-[24px]: min-h-screen">
+      <div className="">
+        <Sidebar />
+      </div>
       <div className="">
         <Dashboard />
       </div>
