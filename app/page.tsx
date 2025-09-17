64a06d47c6e85b2b9a3cf7cd9688cfe5d2292155
victor-kofa <victor.owusu@kofa.co>
2025-09-17 09:47:58 +0000

    add click button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
+import Link from "next/link";
 import Sidebar from "./(featured)/dashboard/components/Sidebar";
 import Dashboard from "./(featured)/dashboard/page";
 
 export default function Home() {
   return (
-    <section className="flex max-[24px]: min-h-screen">
+    <section className="flex max-[24px] min-h-screen">
       <div className="">
         <Sidebar />
       </div>
       <div className="">
         <Dashboard />
+        <div className="">
+          <Link
+            href="/dashboard-2"
+            className="flex justify-center items-center capitalize py-1 px-1"
+          >
+            <h1 className=" text-center mt-2 rounded-md border w-1/4 hover:bg-gray-300">
+              click to view Dashboard 2
+            </h1>
+          </Link>
+        </div>
       </div>
     </section>
   );
