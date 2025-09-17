87f0917e7f32e84e93d305f2cf615e34dc269e2a
victor-kofa <victor.owusu@kofa.co>
2025-09-17 09:49:21 +0000

    add boarder

--- app/(featured)/dashboard/components/Header.tsx
+++ app/(featured)/dashboard/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Header() {
-  return <header className="navbar text-center pt-4">header</header>;
+  return <header className="navbar border border-b-4 w-full h-[64px] text-center pt-4">header</header>;
 }
