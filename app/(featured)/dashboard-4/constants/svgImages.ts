a79e89bc66d6abf0b25eadc9d30563f7474a4412
victor-kofa <victor.owusu@kofa.co>
2025-09-18 09:37:33 +0000

    svg icons for dashboard 4 and 5

---
+++ app/(featured)/dashboard-4/constants/svgImages.ts
@@ -0,0 +1,22 @@
+export const DASHBOARD4 = "/assets/svgs/dashboard.svg";
+
+export const MOTORBIKE4 = "/assets/svgs/white-motor.svg";
+
+export const COMPASS4 = "/assets/svgs/white-compass.svg";
+
+export const ADMIN4 = "/assets/svgs/white-user.svg";
+
+export const LOGO4 = "/assets/svgs/white-logo.svg";
+
+export const SIDEBAR4 = "/assets/svgs/white-sidebar.svg";
+
+export const RIDERS4 = "/assets/svgs/white-riders.svg";
+
+// BLUE-BLACK ICONS
+export const DASHBOARD5 = "/assets/svgs/blue-black-dashboard.svg";
+
+export const MOTORBIKE5 = "/assets/svgs/blue-black-motorbike.svg";
+
+export const COMPASS5 = "/assets/svgs/blue-black-compass.svg";
+
+export const ADMIN5 = "/assets/svgs/blue-black-user.svg";
