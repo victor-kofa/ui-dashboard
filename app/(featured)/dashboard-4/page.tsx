de256cf7b8006393953b061e987fdfba782b9559
victor-kofa <victor.owusu@kofa.co>
2025-09-18 08:54:18 +0000

    default  dashboard

---
+++ app/(featured)/dashboard-4/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import SearchAdd from "./components/SearchAdd";
+import DashboardTable from "../dashboard/components/DashboardTable";
+import Link from "next/link";
+
+export default function Dashboard() {
+  return (
+    <div className="flex justify-between">
+      <div className="">
+        <Sidebar />
+      </div>
+
+      <div className="mx-[24px]">
+        <Header />
+        <SearchAdd />
+        <DashboardTable />
+
+        <div className="">
+          <Link
+            href="/dashboard-5"
+            className="flex justify-center items-center capitalize py-1 px-1"
+          >
+            <h1 className=" text-center mt-2 rounded-md border w-1/4 hover:bg-gray-300">
+              click to view Dashboard 5
+            </h1>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
