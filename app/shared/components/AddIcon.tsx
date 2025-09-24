de0f8abbb444080f216ac1564cd89d8677e05236
victor-kofa <victor.owusu@kofa.co>
2025-09-24 17:46:57 +0000

    add default alt

--- app/shared/components/AddIcon.tsx
+++ app/shared/components/AddIcon.tsx
@@ -3,22 +3,23 @@ import Image from "next/image";
 interface AddIconProps {
   iconSize?: number;
   className?: string;
+  alt?: string;
   width?: number; // precise CSS width in px
   height?: number; // precise CSS height in px
   angle?: string | number; // rotation angle, e.g. '0deg' or 0
   path: string;
 }
 export const AddIcon: React.FC<AddIconProps> = ({
-
   className,
   width,
   height,
   path,
+  alt,
 }) => {
   return (
     <Image
       src={path}
-      alt="add icon"
+      alt={alt || "add icon"}
       className={className}
       width={width}
       height={height}
